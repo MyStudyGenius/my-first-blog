4b8f9648a3dac885fd4ccdab7c26ab8f862f306a
benjamin flum <benjamin.flum@mystudygenius.com>
2019-03-23 20:10:30 +0100

    Possible to create posts now

---
+++ blog/forms.py
@@ -0,0 +1,12 @@
+"""blog form Configuration
+
+"""
+from django import forms
+
+from .models import Post
+
+class PostForm(forms.ModelForm):
+
+    class Meta:
+        model = Post
+        fields = ('title', 'text',)
